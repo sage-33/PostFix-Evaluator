64f124053236bb74991d134c517a5374f4fa09f9
Sage Silberman <ssilberman2023@chadwickschool.org>
2022-01-10 18:43:18 -0800

    feat: Implement LinkedStack

--- src/main/java/stack/LinkedStack.java
+++ src/main/java/stack/LinkedStack.java
@@ -1,56 +1,101 @@
 package stack;
 
 /**
- * A {@link LinkedStack} is a stack that is implemented using a Linked List structure
- * to allow for unbounded size.
- *
+ * A {@link LinkedStack} is a stack that is implemented using a Linked List
+ * structure to allow for unbounded size.
+ * 
  * @param <T> the elements stored in the stack
  */
 public class LinkedStack<T> implements StackInterface<T> {
 
-  /**
-   * {@inheritDoc}.
-   */
-  @Override
-  public T pop() throws StackUnderflowException {
-    // TODO Auto-generated method stub
-    return null;
-  }
+	/** indicates number of elements stored */
+	private int count;
+	/** pointer to top of stack */
+	private LinearNode<T> top;
 
-  /**
-   * {@inheritDoc}.
-   */
-  @Override
-  public T top() throws StackUnderflowException {
-    // TODO Auto-generated method stub
-    return null;
-  }
+	public LinkedStack() {
+		count = 0;
+		top = null;
 
-  /**
-   * {@inheritDoc}.
-   */
-  @Override
-  public boolean isEmpty() {
-    // TODO Auto-generated method stub
-    return false;
-  }
+	}
 
-  /**
-   * {@inheritDoc}.
-   */
-  @Override
-  public int size() {
-    // TODO Auto-generated method stub
-    return 0;
-  }
+	/**
+	 * Removes the element at the top of this stack and returns a reference to it.
+	 * Throws an EmptyCollectionException if the stack is empty.
+	 * 
+	 * @return T element from top of stack
+	 * @throws EmptyStackException on pop from empty stack {@inheritDoc}.
+	 */
+	@Override
+	public T pop() throws StackUnderflowException {
+		if (isEmpty())
+			throw new StackUnderflowException("Stack");
 
-  /**
-   * {@inheritDoc}.
-   */
-  @Override
-  public void push(T elem) {
-    // TODO Auto-generated method stub
+		T result = top.getElement();
+		top = top.getNext();
+		count--;
 
-  }
+		return result;
+	}
 
+	/**
+	 * Returns the top most element of this stack.
+	 * 
+	 * @return the top most element of this stack.
+	 * @throws StackUnderflowException if the stack is empty
+	 * 
+	 *                                 {@inheritDoc}.
+	 */
+	@Override
+	public T top() throws StackUnderflowException {
+		if (isEmpty())
+			throw new StackUnderflowException("Stack");
+
+		return top.getElement();
+
+	}
+
+	/**
+	 * 
+	 * Returns {@code true} if the stack contains no elements and {@code false}
+	 * otherwise.
+	 * 
+	 * @return {@code true} if the stack contains no elements and {@code false}
+	 *         otherwise. {@inheritDoc}.
+	 */
+	@Override
+	public boolean isEmpty() {
+		if (count == 0) {
+			return true;
+
+		} else {
+			return false;
+
+		}
+	}
+
+	/**
+	 * Returns the number of elements in this stack.
+	 * 
+	 * @return the number of elements in this stack. {@inheritDoc}.
+	 */
+	@Override
+	public int size() {
+		return count;
+	}
+
+	/**
+	 * Adds the specified element to the top of this stack.
+	 * 
+	 * @param element element to be pushed on stack
+	 * 
+	 *                {@inheritDoc}.
+	 */
+	@Override
+	public void push(T elem) {
+		LinearNode<T> temp = new LinearNode<T>(elem);
+		temp.setNext(top);
+		top = temp;
+		count++;
+	}
 }
--- src/test/java/stack/LinkedStackTest.java
+++ src/test/java/stack/LinkedStackTest.java
@@ -9,47 +9,74 @@ import org.junit.Test;
 
 public class LinkedStackTest {
 
-  private StackInterface<Integer> stack;
+	private StackInterface<Integer> stack;
 
-  @Before
-  public void setup() {
-    stack = new LinkedStack<Integer>();
-  }
+	private StackInterface<String> stringStack;
 
-  @Test (timeout = 5000)
-  public void testStack() {
-    assertTrue("Stack should be empty after being constructed.", stack.isEmpty());
-    assertEquals("Stack should contain zero elements after being constructed.", 0, stack.size());
+	@Before
+	public void setup() {
+		stack = new LinkedStack<Integer>();
+		stringStack = new LinkedStack<String>();
 
-    stack.push(5);
-    assertFalse("Stack should not be empty.", stack.isEmpty());
-    assertEquals("The top element should be 5", new Integer(5), stack.top());
-    assertEquals("The stack should contain one element.", 1, stack.size());
+	}
 
-    stack.push(4);
-    assertEquals("The top element should be 4", new Integer(4), stack.top());
-    assertEquals("The stack should contain two elements.", 2, stack.size());
+	@Test(timeout = 5000)
+	public void testStack() {
+		assertTrue("Stack should be empty after being constructed.", stack.isEmpty());
+		assertEquals("Stack should contain zero elements after being constructed.", 0, stack.size());
 
-    Integer t = stack.pop();
-    assertEquals("The popped element should be 4", new Integer(4), t);
-    assertEquals("The top element should be 5", new Integer(5), stack.top());
-    assertEquals("The stack should contain one element.", 1, stack.size());
-    assertFalse("The stack should not be empty.", stack.isEmpty());
+		stack.push(5);
+		assertFalse("Stack should not be empty.", stack.isEmpty());
+		assertEquals("The top element should be 5", new Integer(5), stack.top());
+		assertEquals("The stack should contain one element.", 1, stack.size());
 
-    t = stack.pop();
-    assertEquals("The popped element should be 5", new Integer(5), t);
-    assertTrue("The stack should be empty.", stack.isEmpty());
-  }
+		stack.push(4);
+		assertEquals("The top element should be 4", new Integer(4), stack.top());
+		assertEquals("The stack should contain two elements.", 2, stack.size());
 
-  @Test (timeout = 5000, expected = StackUnderflowException.class)
-  public void testStackUnderflowPop() {
-    stack.pop();
-  }
+		Integer t = stack.pop();
+		assertEquals("The popped element should be 4", new Integer(4), t);
+		assertEquals("The top element should be 5", new Integer(5), stack.top());
+		assertEquals("The stack should contain one element.", 1, stack.size());
+		assertFalse("The stack should not be empty.", stack.isEmpty());
 
-  @Test (timeout = 5000, expected = StackUnderflowException.class)
-  public void testStackUnderflowTop() {
-    stack.top();
-  }
+		t = stack.pop();
+		assertEquals("The popped element should be 5", new Integer(5), t);
+		assertTrue("The stack should be empty.", stack.isEmpty());
+	}
 
+	@Test(timeout = 5000)
+	public void testStringStack() {
+		assertTrue("Stack should be empty after being constructed.", stringStack.isEmpty());
+		assertEquals("Stack should contain zero elements after being constructed.", 0, stringStack.size());
 
+		stringStack.push("cat");
+		assertFalse("Stack should not be empty.", stringStack.isEmpty());
+		assertEquals("The top element should be cat", new String("cat"), stringStack.top());
+		assertEquals("The stack should contain one elemnt.", 1, stringStack.size());
+
+		stringStack.push("dog");
+		assertEquals("The top element should be dog", new String("dog"), stringStack.top());
+		assertEquals("The stack should contain two elements.", 2, stringStack.size());
+
+		String t = stringStack.pop();
+		assertEquals("The popped element should be dog", new String("dog"), t);
+		assertEquals("The top element should be cat", new String("cat"), stringStack.top());
+		assertEquals("The stack should contain one element.", 1, stringStack.size());
+		assertFalse("The stack should not be empty.", stringStack.isEmpty());
+
+		t = stringStack.pop();
+		assertEquals("The popped element should be cat", new String("cat"), t);
+		assertTrue("The stack should be empty.", stringStack.isEmpty());
+	}
+
+	@Test(timeout = 5000, expected = StackUnderflowException.class)
+	public void testStackUnderflowPop() {
+		stack.pop();
+	}
+
+	@Test(timeout = 5000, expected = StackUnderflowException.class)
+	public void testStackUnderflowTop() {
+		stack.top();
+	}
 }
